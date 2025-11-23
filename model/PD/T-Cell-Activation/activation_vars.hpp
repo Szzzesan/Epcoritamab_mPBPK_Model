1d3ced144fe39dbce2a835df730e5d79d3d800d5
Szzzesan <shichenzhang89@gmail.com>
2025-11-22 19:20:54 -0500

    removed clamping

--- model/PD/T-Cell-Activation/activation_vars.hpp
+++ model/PD/T-Cell-Activation/activation_vars.hpp
@@ -7,7 +7,7 @@ double Trimer_per_Tumor = 0.0;
 
 if (TUMOR_CELLS > 1.0) {
   // Logic: Trimers in Node / Number of Tumor Cells
-  Trimer_per_Tumor = (CLAMP(TRIMER_NODE) / nmol_per_molecule) / TUMOR_CELLS;
+  Trimer_per_Tumor = (TRIMER_NODE / nmol_per_molecule) / TUMOR_CELLS;
 }
 
 // Stepwise Activation Function (RELU)
@@ -24,10 +24,10 @@ if (TAFD > TAD) {
 // 2. Activation against B-Cells (Systemic)
 // Rate = sim_slope * (Trimers / B-Cells) in that specific tissue
 
-double Trimer_per_BC_BL = (CLAMP(BC_BLOOD) > 1)  ? (CLAMP(TRIMER_BLOOD) / nmol_per_molecule) / BC_BLOOD  : 0.0;
-double Trimer_per_BC_SP = (CLAMP(BC_SPLEEN) > 1) ? (CLAMP(TRIMER_SPLEEN) / nmol_per_molecule) / BC_SPLEEN : 0.0;
-double Trimer_per_BC_LN = (CLAMP(BC_NODE) > 1)   ? (CLAMP(TRIMER_NODE) / nmol_per_molecule) / BC_NODE   : 0.0;
-double Trimer_per_BC_LY = (CLAMP(BC_LYMPH) > 1)  ? (CLAMP(TRIMER_LYMPH) / nmol_per_molecule) / BC_LYMPH  : 0.0;
+double Trimer_per_BC_BL = (BC_BLOOD > 1)  ? (TRIMER_BLOOD / nmol_per_molecule) / BC_BLOOD  : 0.0;
+double Trimer_per_BC_SP = (BC_SPLEEN > 1) ? (TRIMER_SPLEEN / nmol_per_molecule) / BC_SPLEEN : 0.0;
+double Trimer_per_BC_LN = (BC_NODE > 1)   ? (TRIMER_NODE / nmol_per_molecule) / BC_NODE   : 0.0;
+double Trimer_per_BC_LY = (BC_LYMPH > 1)  ? (TRIMER_LYMPH / nmol_per_molecule) / BC_LYMPH  : 0.0;
 
 double rate_act_BC_BL = (TAFD > TAD) ? sim_slope * Trimer_per_BC_BL : 0.0;
 double rate_act_BC_SP = (TAFD > TAD) ? sim_slope * Trimer_per_BC_SP : 0.0;
@@ -36,24 +36,24 @@ double rate_act_BC_LY = (TAFD > TAD) ? sim_slope * Trimer_per_BC_LY : 0.0;
 
 // 3. Trafficking in and out
 // (1) vATC_BC Trafficking Flows
-double flow_vATC_BL_SP = kpt * (1 + INJ_Scaler * INJ) * CLAMP(vATC_BC_BLOOD);
-double flow_vATC_SP_LN = ktn * CLAMP(vATC_BC_SPLEEN);
-double flow_vATC_LN_LY = knl * CLAMP(vATC_BC_NODE); // Use 1672
-double flow_vATC_LY_BL = klp * CLAMP(vATC_BC_LYMPH);
+double flow_vATC_BL_SP = kpt * (1 + INJ_Scaler * INJ) * vATC_BC_BLOOD;
+double flow_vATC_SP_LN = ktn * vATC_BC_SPLEEN;
+double flow_vATC_LN_LY = knl * vATC_BC_NODE; // Use 1672
+double flow_vATC_LY_BL = klp * vATC_BC_LYMPH;
 
 // (2)pATC Flows
-double flow_pATC_BL_SP = kpt * (1 + INJ_Scaler * INJ) * CLAMP(pATC_BLOOD);
-double flow_pATC_SP_LN = ktn * CLAMP(pATC_SPLEEN);
-double flow_pATC_LN_LY = knl * CLAMP(pATC_NODE);
-double flow_pATC_LY_BL = klp * CLAMP(pATC_LYMPH);
+double flow_pATC_BL_SP = kpt * (1 + INJ_Scaler * INJ) * pATC_BLOOD;
+double flow_pATC_SP_LN = ktn * pATC_SPLEEN;
+double flow_pATC_LN_LY = knl * pATC_NODE;
+double flow_pATC_LY_BL = klp * pATC_LYMPH;
 
 // 4. Expansion & Death Rates
 // Activated cells multiply!
 // Expansion depends on local activated cells
-double exp_BL_rate = expand_factor * CLAMP(vATC_BC_BLOOD);
-double exp_SP_rate = expand_factor * CLAMP(vATC_BC_SPLEEN);
+double exp_BL_rate = expand_factor * vATC_BC_BLOOD;
+double exp_SP_rate = expand_factor * vATC_BC_SPLEEN;
 // Node has BOTH types of vATC contributing to expansion!
-double exp_LN_rate = expand_factor * (CLAMP(vATC_BC_NODE) + CLAMP(vATC_TUMOR_NODE)); 
-double exp_LY_rate = expand_factor * CLAMP(vATC_BC_LYMPH);
+double exp_LN_rate = expand_factor * (vATC_BC_NODE + vATC_TUMOR_NODE); 
+double exp_LY_rate = expand_factor * vATC_BC_LYMPH;
 
 double rate_death_ATC = (TAFD > (TAD + Tp)) ? koutATC : 0.0;
--- model/PD/Trafficking/traff_vars.hpp
+++ model/PD/Trafficking/traff_vars.hpp
@@ -27,27 +27,27 @@
 // T-Cells: Production & Death
 // We also clamp AF_TC to prevent errors if homeostasis dips slightly below 0
 double prod_TC         = kinTC * pow(CLAMP(AF_TC), r);
-double death_TC_blood  = koutTC * CLAMP(TC_BLOOD);
-double death_TC_spleen = koutTC * CLAMP(TC_SPLEEN);
-double death_TC_node   = koutTC * CLAMP(TC_NODE);
-double death_TC_lymph  = koutTC * CLAMP(TC_LYMPH);
+double death_TC_blood  = koutTC * TC_BLOOD;
+double death_TC_spleen = koutTC * TC_SPLEEN;
+double death_TC_node   = koutTC * TC_NODE;
+double death_TC_lymph  = koutTC * TC_LYMPH;
 
 // T-Cells: Trafficking Fluxes
 // Logic: We can only move cells that actually exist
-double traf_TC_blood_spleen = kpt * (1 + INJ_Scaler * INJ) * CLAMP(TC_BLOOD);
-double traf_TC_spleen_node  = ktn * CLAMP(TC_SPLEEN);
-double traf_TC_node_lymph   = knl * CLAMP(TC_NODE);
-double traf_TC_lymph_blood  = klp * CLAMP(TC_LYMPH);
+double traf_TC_blood_spleen = kpt * (1 + INJ_Scaler * INJ) * TC_BLOOD;
+double traf_TC_spleen_node  = ktn * TC_SPLEEN;
+double traf_TC_node_lymph   = knl * TC_NODE;
+double traf_TC_lymph_blood  = klp * TC_LYMPH;
 
 // B-Cells: Production & Death
 double prod_BC         = kinBC * pow(CLAMP(AF_BC), r);
-double death_BC_blood  = koutBC * CLAMP(BC_BLOOD);
-double death_BC_spleen = koutBC * CLAMP(BC_SPLEEN);
-double death_BC_node   = koutBC * CLAMP(BC_NODE);
-double death_BC_lymph  = koutBC * CLAMP(BC_LYMPH);
+double death_BC_blood  = koutBC * BC_BLOOD;
+double death_BC_spleen = koutBC * BC_SPLEEN;
+double death_BC_node   = koutBC * BC_NODE;
+double death_BC_lymph  = koutBC * BC_LYMPH;
 
 // B-Cells: Trafficking Fluxes
-double traf_BC_blood_spleen = kpt * (1 + INJ_Scaler * INJ) * CLAMP(BC_BLOOD);
-double traf_BC_spleen_node  = ktn * CLAMP(BC_SPLEEN);
-double traf_BC_node_lymph   = knl * CLAMP(BC_NODE);
-double traf_BC_lymph_blood  = klp * CLAMP(BC_LYMPH);
+double traf_BC_blood_spleen = kpt * (1 + INJ_Scaler * INJ) * BC_BLOOD;
+double traf_BC_spleen_node  = ktn * BC_SPLEEN;
+double traf_BC_node_lymph   = knl * BC_NODE;
+double traf_BC_lymph_blood  = klp * BC_LYMPH;
